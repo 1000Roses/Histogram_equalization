46e81a9d94273716a13d17b343ec9e00352409e6
Lê Tiến <45195540+1000Roses@users.noreply.github.com>
2019-04-10 22:06:08 +0700

    Update HistogramEqual.py

--- Histogram_Equalization/Src/HistogramEqual.py
+++ Histogram_Equalization/Src/HistogramEqual.py
@@ -1,12 +1,12 @@
-import cv2, os,sys							#|
-import numpy as np		   					#|		#Import 
-import collections							#|
+import cv2, os,sys				#|
+import numpy as np				#|		#Import 
+import collections				#|
 from matplotlib import pyplot as plt		#|
 #--------------------------------------------
 
-Open_Image = sys.argv[1]										#		
+Open_Image = sys.argv[1]					#		
 assert os.path.isfile(Open_Image), '%s not found' % Open_Image	# Code to open your image
-img = cv2.imread(Open_Image)									#
+img = cv2.imread(Open_Image)					#
 
 #----------------------------------------------
 def create_histogram(img,output = "input"):
@@ -37,20 +37,20 @@ AllOfPixels = img.shape[0]*img.shape[1]*3	#|		#Code to find all of Pixels in ima
 #---------------------------------------------
 
 #----------------------------------------------------------------------------------------------------
-k = 0;																								#|																							#|																										#|
-for i in c :					    #--------	VanTien	@Author	--------							#|
-	if (k >= 1):																					#|
-		c[i] = c[i] + Temp																			#|
-	Temp = c[i]																						#| 		#Code Equalization.
-	k = k + 1																						#|
-for j in c :																						#|
-	c[j]= round(((c[j] - cdf_min)/(AllOfPixels - cdf_min)) * 255) #value			       			    #| 
-	if (c[j] > 255):												#|  
-		c[j] = 255												#| =====     ====    ===========
-for row in range(img.shape[0]):			 									#|   ====   ====         ===
-	for col in range(img.shape[1]):											#|    ==== ====          ===
-		for step in range(3):											#|     =======    <:>    ===
-			img[row][col][step] = c[img[row][col][step]]							#|
+k = 0;													#|																							#|																										#|
+for i in c :					    #--------	VanTien	@Author	--------		#|
+	if (k >= 1):											#|
+		c[i] = c[i] + Temp									#|
+	Temp = c[i]				    							#| 		#Code Equalization.
+	k = k + 1											#|
+for j in c :												#|
+	c[j]= round(((c[j] - cdf_min)/(AllOfPixels - cdf_min)) * 255) #value		        	#|
+	if (c[j] > 255):										#|  
+		c[j] = 255										#| =====     ====    ===========
+for row in range(img.shape[0]):			 							#|   ====   ====         ===
+	for col in range(img.shape[1]): 								#|    ==== ====          ===
+		for step in range(3):									#|     =======    <:>    ===
+			img[row][col][step] = c[img[row][col][step]]					#|
 #-----------------------------------------------------------------------------------------------------
 
 create_histogram(img,output = "After_Equalization")  #print histogram of image after equalization
